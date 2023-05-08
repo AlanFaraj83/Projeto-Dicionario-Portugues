6ce4375a40fa16b7f05dfcb27aeaf28357e046ee
AlanFaraj83 <alanfaraj@outlook.com>
2023-05-08 18:09:16 -0300

    :lipstick: feat: Atualizando a pagina script.js

--- script/script.js
+++ script/script.js
@@ -2,3 +2,4 @@ let url = 'https://dicio-api-ten.vercel.app/v2/';
 
 let inputTxt = document.querySelector('#container_inputtxt');
 let btnTxt = document.querySelector('#container_btn');
+let resultado = document.querySelector('#container_result');
