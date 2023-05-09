6cb2036e1dc3f1d2c6730e0e1242bfaec38b0aa2
AlanFaraj83 <alanfaraj@outlook.com>
2023-05-09 15:41:02 -0300

    :sparkles:Atualizando  o script.js

--- script/script.js
+++ script/script.js
@@ -3,3 +3,12 @@ let url = 'https://dicio-api-ten.vercel.app/v2/';
 let inputTxt = document.querySelector('#container_inputtxt');
 let btnTxt = document.querySelector('#container_btn');
 let resultado = document.querySelector('#container_result');
+
+btnTxt.addEventListener('click', () => {
+    let palavra = inputTxt.value;
+    if (palavra ==='') {
+        alert('Nada foi escrito');
+    } else {
+        fetch(`${url}${palavra}`).then((resposta) => console.log(resposta.json()))
+    }
+})
