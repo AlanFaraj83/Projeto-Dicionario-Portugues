c3f2fe78eef099de38297d9b86ed32a399ff81a8
AlanFaraj83 <alanfaraj@outlook.com>
2023-05-09 16:09:57 -0300

    :sparkles:Atualizando  o script.js

--- script/script.js
+++ script/script.js
@@ -17,7 +17,8 @@ btnTxt.addEventListener('click', () => {
                 `<h3 id="container_palavra">${palavra}</h3>
                 <p id="container-significa"><span>1°</span>${data[0].meanings[0]}</p>
                 <p id="container-significa"><span>2°</span>${data[0].meanings[1]}</p>
-                <p id="container-significa"><span>3°</span>${data[0].meanings[2]}</p>`
+                <p id="container-significa"><span>3°</span>${data[0].meanings[2]}</p>
+                <p id="container-significa"><span>4°</span>${data[0].meanings[3]}</p>`
 
             }
 
